4bcfc2909f633c9c8afc6f3d99b7eebd2aea9c0d
lAvarisel <60235054+lAvarisel@users.noreply.github.com>
2020-02-28 16:13:12 +0300

    bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -27,19 +27,27 @@ $("nav, .promo-btns-container").on("click","a", function (event) {
 });
 
 $("#navToggle").click(function() {
+    $('#menu').toggleClass("overlayMenu");
+    $('.social-container').toggleClass("mobile");
     $(this).toggleClass("active");
-    $(".overlay").toggleClass("open");
+    $(".nav-container").toggleClass("overlay");
     // this line ▼ prevents content scroll-behind
     $("body").toggleClass("locked");
 });
-$('.overlay').click(function() {
+
+$('.nav-container').click(function() {
     $(this).removeClass('open');
     $('.navBurger').removeClass('active');
+    $(".nav-container").removeClass("overlay");
+    $("body").removeClass("locked");
+    $('#menu').removeClass("overlayMenu");
+    $('.social-container').removeClass("mobile");
 });
+
 $( () => {
 	$(window).scroll( () => {
 		var windowTop = $(window).scrollTop();
 		windowTop > 100 ? $('header').addClass('navShadow') : $('header').removeClass('navShadow');
-		windowTop > 100 ? $('ul').css('top','100px') : $('ul').css('top','160px');
+		windowTop > 100 ? $('#menu ul').css('top','100px') : $('#menu ul').css('top','160px');
 	});
 });
